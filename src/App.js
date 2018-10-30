5f138385524460f597b6ce2732dc036f4f81ea21
VilhelmNielsen <seb.vilhelm@gmail.com>
2018-10-30 21:57:03 +0100

    More setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { createGlobalStyle } from 'styled-components/macro'
+
+const GlobalStyle = createGlobalStyle`
+html { box-sizing: border-box; font-size: 18px; }
+*,*::before,*::after {box-sizing: inherit;}
+body {
+  font-size: 1rem;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+`
 
 class App extends Component {
   render() {
     return (
-      <div >
+      <div>
+        <GlobalStyle />
         <h1>Wish List</h1>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
